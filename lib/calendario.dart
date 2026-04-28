a67c3fe5cfe4e727e005d877744ab33e628bc765
bruno-rian <brpc1@aluno.ifal.edu.br>
2026-04-27 23:27:27 -0300

    primeiro

--- lib/calendario.dart
+++ lib/calendario.dart
@@ -22,47 +22,99 @@ class _CalendarioState extends State<Calendario> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(padding: EdgeInsets.only(right: 0)),
-                Text(
-                  "Calendário",
-                  style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white
-                  ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(padding: EdgeInsets.only(right: 0)),
+              Text(
+                "Calendário",
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white
                 ),
+              ),
 
-                FloatingActionButton(
-                  onPressed: null,
-                  backgroundColor: Colors.white,
-                  shape: const CircleBorder(),
+              FloatingActionButton(
+                onPressed: null,
+                backgroundColor: Colors.white,
+                shape: const CircleBorder(),
 
-                  child: Icon(
-                      Icons.settings,
-                      size: 25,
-                      color: const Color(0xFF1E3050)
+                child: Icon(
+                    Icons.settings,
+                    size: 25,
+                    color: const Color(0xFF1E3050)
+                ),
+              )
+            ],
+          ),
+          toolbarHeight: 60,
+          centerTitle: true,
+          backgroundColor: const Color(0xFF1E3050)
+      ),
+
+      body: Column(
+        children: [
+          TableCalendar(
+              rowHeight: 80,
+              focusedDay: today,
+              firstDay: DateTime(2015, 01, 01),
+              lastDay: DateTime(2030, 01, 01))
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        padding: EdgeInsets.only(bottom: 10),
+        child: Row (
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                IconButton(onPressed: null, icon: Icon(Icons.notifications_none), iconSize: 30,),
+                Text(
+                  "Lembretes",
+                  style: TextStyle(
+                      fontSize: 11
                   ),
                 )
               ],
             ),
-            toolbarHeight: 60,
-            centerTitle: true,
-            backgroundColor: const Color(0xFF1E3050)
-        ),
-
-        body: Column(
-          children: [
-            TableCalendar(
-                rowHeight: 80,
-                focusedDay: today,
-                firstDay: DateTime(2015, 01, 01),
-                lastDay: DateTime(2030, 01, 01))
+            Column(
+              children: [
+                IconButton(onPressed: null, icon: Icon(Icons.list_alt_outlined), iconSize: 30),
+                Text(
+                  "Atividades",
+                  style: TextStyle(
+                      fontSize: 11
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                IconButton(onPressed: null, icon: Icon(Icons.group_outlined), iconSize: 30,),
+                Text(
+                  "Grupos",
+                  style: TextStyle(
+                      fontSize: 11
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                IconButton(onPressed: null, icon: Icon(Icons.person_outline), iconSize: 30,),
+                Text(
+                  "Perfil",
+                  style: TextStyle(
+                      fontSize: 11
+                  ),
+                )
+              ],
+            ),
           ],
-        )
+        ),
+      ),
     );
   }
 }
