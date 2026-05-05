e7448697e15d8352e4c00d9cb717182d967e0093
euDavi01 <dpbs1@aluno.ifal.edu.br>
2026-05-04 21:20:40 -0300

    commit final

--- lib/telagrupos.dart
+++ lib/telagrupos.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class TelaGrupos extends StatelessWidget {
-  const TelaGrupos({super.key});
+  TelaGrupos({super.key});
 
   @override
   Widget build(BuildContext contexto) {
